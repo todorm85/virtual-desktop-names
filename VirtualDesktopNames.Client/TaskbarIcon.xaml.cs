ecd7674308e4b0ad881332c77982cd7ba8a0d8d5
Todor Mitskovski <todorbm85@gmail.com>
2016-11-23 19:47:49 +0200

    Introduced MVVM pattern

---
+++ VirtualDesktopNames.Client/TaskbarIcon.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace VirtualDesktopNames.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<CancellationTokenSource> cancellationTokens = new List<CancellationTokenSource>();
+
+        private TaskbarIconViewModel viewModel;
+
+        public MainWindow()
+        {
+            this.viewModel = new TaskbarIconViewModel();
+            this.DataContext = this.viewModel;
+            this.viewModel.DesktopChanged += ViewModel_DesktopChanged;
+
+            InitializeComponent();
+        }
+
+        private void ViewModel_DesktopChanged(object sender, EventArgs e)
+        {
+            this.ShowStandardBalloon(this.viewModel.CurrentDesktopName);
+        }
+
+        private void ShowStandardBalloon(string msg)
+        {
+            this.cancellationTokens.ForEach(x => x.Cancel());
+            string title = msg;
+            string text = "...";
+
+            MyNotifyIcon.HideBalloonTip();
+            MyNotifyIcon.ShowBalloonTip(title, text, BalloonIcon.Info);
+
+            var taskCancellationTokenSource = new CancellationTokenSource();
+            this.cancellationTokens.Add(taskCancellationTokenSource);
+            var task = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1500);
+                if (!taskCancellationTokenSource.IsCancellationRequested)
+                {
+                    MyNotifyIcon.HideBalloonTip();
+                }
+            });
+
+            task.GetAwaiter().OnCompleted(() =>
+            {
+                task.Dispose();
+                this.cancellationTokens.Remove(taskCancellationTokenSource);
+                taskCancellationTokenSource.Dispose();
+            });
+        }
+    }
+}
---
+++ VirtualDesktopNames.Client/TaskbarIconViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using VirtualDesktopNames.Client.MVVMBaseClasses;
+
+namespace VirtualDesktopNames.Client
+{
+    public class TaskbarIconViewModel : ObservableObject
+    {
+        private string currentDesktopName;
+        private VirtualDesktopsManager vdm;
+
+        public TaskbarIconViewModel()
+        {
+            this.vdm = new VirtualDesktopsManager();
+            this.CurrentDesktopName = this.vdm.GetCurrentDesktopName();
+            vdm.OnDesktopChanged += Vdm_OnDesktopChanged;
+        }
+
+        public string CurrentDesktopName
+        {
+            get { return currentDesktopName; }
+            set
+            {
+                this.RaisePropertyChangedEvent("CurrentDesktopName");
+                currentDesktopName = value;
+            }
+        }
+
+        public ICommand SetNameCommand
+        {
+            get { return new DelegateCommand(this.SetDesktopName); }
+        }
+
+        private void SetDesktopName()
+        {
+            DesktopNameChange inputDialog = new DesktopNameChange("Please enter current desktop name:", this.vdm.GetCurrentDesktopName());
+            if (inputDialog.ShowDialog() == true)
+            {
+                this.vdm.SetCurrentDesktopName(inputDialog.Answer);
+                this.CurrentDesktopName = this.vdm.GetCurrentDesktopName();
+            }
+        }
+
+        public ICommand ExitCommand
+        {
+            get { return new DelegateCommand(this.Exit); }
+        }
+
+        private void Exit()
+        {
+            App.Current.Shutdown();
+        }
+
+        private void Vdm_OnDesktopChanged(object sender, System.EventArgs e)
+        {
+            this.CurrentDesktopName = this.vdm.GetCurrentDesktopName();
+            this.RaiseDesktopChanged();
+        }
+
+        public event EventHandler DesktopChanged;
+
+        protected void RaiseDesktopChanged()
+        {
+            this.DesktopChanged?.Invoke(this, new EventArgs());
+        }
+    }
+}
